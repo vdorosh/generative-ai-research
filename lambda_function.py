c6252259970626031b0345494f6ec6152a60d895
Vasyl Doroshenko <vdoro@softserveinc.com>
2023-07-24 15:52:09 +0300

    Add variables validation

--- lambda_function.py
+++ lambda_function.py
@@ -2,15 +2,24 @@ import boto3
 import json
 import os
 import logging
+from datetime import datetime
 
 # Set up logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-session = boto3.Session()
+def get_ec2_resource():
+    try:
+        # If AWS_PROFILE environment variable is set, use it
+        profile = os.environ['AWS_PROFILE']
+        session = boto3.Session(profile_name=profile)
+    except KeyError:
+        # If not, default to the instance's IAM role
+        session = boto3.Session()
+    return session.resource('ec2')
 
-ec2_resource = session.resource('ec2')
-s3_resource = session.resource('s3')
+ec2_resource = get_ec2_resource()
+s3_resource = boto3.resource('s3')
 
 def get_unattached_volumes():
     unattached_volumes = []
@@ -30,8 +39,12 @@ def get_unencrypted_volumes_and_snapshots():
             unencrypted_snapshots.append(snapshot)
     return unencrypted_volumes, unencrypted_snapshots
 
-def lambda_handler(event, context):
+def main():
     try:
+        # Check if BUCKET_NAME environment variable is set
+        if 'BUCKET_NAME' not in os.environ:
+            raise Exception('BUCKET_NAME environment variable is not set')
+
         logger.info("Getting unattached volumes...")
         unattached_volumes = get_unattached_volumes()
         logger.info("Getting unencrypted volumes and snapshots...")
@@ -57,16 +70,25 @@ def lambda_handler(event, context):
         metrics_json = json.dumps(metrics)
 
         # Write the metrics to a file
+        timestamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        filename = f'/tmp/metrics-{timestamp}.json'
         logger.info("Writing metrics to a file...")
-        with open('/tmp/metrics.json', 'w') as file:
+        with open(filename, 'w') as file:
             file.write(metrics_json)
 
         # Upload the file to an S3 bucket
         bucket_name = os.environ['BUCKET_NAME']  # Get the bucket name from environment variables
         logger.info(f"Uploading file to S3 bucket {bucket_name}...")
-        s3_resource.Bucket(bucket_name).upload_file(Filename='/tmp/metrics.json', Key='metrics.json')
+        s3_resource.Bucket(bucket_name).upload_file(Filename=filename, Key=f'metrics-{timestamp}.json')
         logger.info("File uploaded successfully.")
 
     except Exception as e:
         logger.error(f"An error occurred: {e}")
         raise
+
+def lambda_handler(event, context):
+    main()
+
+if __name__ == "__main__":
+    print("Running locally...")
+    main()
