53d21f423e40bd95ed027062f8bff3842c149627
Vasyl Doroshenko <vdoro@softserveinc.com>
2023-07-24 15:39:41 +0300

    Add lambda_handler as entry point. Move bucket name to env variables

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,13 @@
 import boto3
 import json
+import os
+import logging
 
-session = boto3.Session(profile_name='softserve-sso')
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+session = boto3.Session()
 
 ec2_resource = session.resource('ec2')
 s3_resource = session.resource('s3')
@@ -24,36 +30,43 @@ def get_unencrypted_volumes_and_snapshots():
             unencrypted_snapshots.append(snapshot)
     return unencrypted_volumes, unencrypted_snapshots
 
-try:
-    unattached_volumes = get_unattached_volumes()
-    unencrypted_volumes, unencrypted_snapshots = get_unencrypted_volumes_and_snapshots()
+def lambda_handler(event, context):
+    try:
+        logger.info("Getting unattached volumes...")
+        unattached_volumes = get_unattached_volumes()
+        logger.info("Getting unencrypted volumes and snapshots...")
+        unencrypted_volumes, unencrypted_snapshots = get_unencrypted_volumes_and_snapshots()
 
-    unattached_volumes_count = len(unattached_volumes)
-    unencrypted_volumes_count = len(unencrypted_volumes)
-    unencrypted_snapshots_count = len(unencrypted_snapshots)
+        unattached_volumes_count = len(unattached_volumes)
+        unencrypted_volumes_count = len(unencrypted_volumes)
+        unencrypted_snapshots_count = len(unencrypted_snapshots)
 
-    unattached_volumes_size = sum([volume.size for volume in unattached_volumes])
-    unencrypted_volumes_size = sum([volume.size for volume in unencrypted_volumes])
-    unencrypted_snapshots_size = sum([snapshot.volume_size for snapshot in unencrypted_snapshots])
+        unattached_volumes_size = sum([volume.size for volume in unattached_volumes])
+        unencrypted_volumes_size = sum([volume.size for volume in unencrypted_volumes])
+        unencrypted_snapshots_size = sum([snapshot.volume_size for snapshot in unencrypted_snapshots])
 
-    metrics = {
-        "unattached_volumes_count": unattached_volumes_count,
-        "unattached_volumes_size": unattached_volumes_size,
-        "unencrypted_volumes_count": unencrypted_volumes_count,
-        "unencrypted_volumes_size": unencrypted_volumes_size,
-        "unencrypted_snapshots_count": unencrypted_snapshots_count,
-        "unencrypted_snapshots_size": unencrypted_snapshots_size,
-    }
+        metrics = {
+            "unattached_volumes_count": unattached_volumes_count,
+            "unattached_volumes_size": unattached_volumes_size,
+            "unencrypted_volumes_count": unencrypted_volumes_count,
+            "unencrypted_volumes_size": unencrypted_volumes_size,
+            "unencrypted_snapshots_count": unencrypted_snapshots_count,
+            "unencrypted_snapshots_size": unencrypted_snapshots_size,
+        }
 
-    metrics_json = json.dumps(metrics)
+        metrics_json = json.dumps(metrics)
 
-    # Write the metrics to a file
-    with open('metrics.json', 'w') as file:
-        file.write(metrics_json)
+        # Write the metrics to a file
+        logger.info("Writing metrics to a file...")
+        with open('/tmp/metrics.json', 'w') as file:
+            file.write(metrics_json)
 
-    # Upload the file to an S3 bucket
-    bucket_name = 'your-bucket-name'  # Replace with your bucket name
-    s3_resource.Bucket(bucket_name).upload_file(Filename='metrics.json', Key='metrics.json')
+        # Upload the file to an S3 bucket
+        bucket_name = os.environ['BUCKET_NAME']  # Get the bucket name from environment variables
+        logger.info(f"Uploading file to S3 bucket {bucket_name}...")
+        s3_resource.Bucket(bucket_name).upload_file(Filename='/tmp/metrics.json', Key='metrics.json')
+        logger.info("File uploaded successfully.")
 
-except Exception as e:
-    print(f"An error occurred: {e}")
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        raise
