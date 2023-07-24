d53a2a96e2d24341bbee2369123fa1d05014ea1b
Vasyl Doroshenko <vdoro@softserveinc.com>
2023-07-24 15:39:02 +0300

    Add s3 for storing function output and exception handling

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,10 @@
 import boto3
+import json
 
-# Create a session using your AWS credentials
-session = boto3.Session(
-    aws_access_key_id='YOUR_ACCESS_KEY',
-    aws_secret_access_key='YOUR_SECRET_KEY',
-    region_name='YOUR_REGION_NAME' # e.g. 'us-west-2'
-)
+session = boto3.Session(profile_name='softserve-sso')
 
 ec2_resource = session.resource('ec2')
+s3_resource = session.resource('s3')
 
 def get_unattached_volumes():
     unattached_volumes = []
@@ -27,20 +24,36 @@ def get_unencrypted_volumes_and_snapshots():
             unencrypted_snapshots.append(snapshot)
     return unencrypted_volumes, unencrypted_snapshots
 
-unattached_volumes = get_unattached_volumes()
-unencrypted_volumes, unencrypted_snapshots = get_unencrypted_volumes_and_snapshots()
+try:
+    unattached_volumes = get_unattached_volumes()
+    unencrypted_volumes, unencrypted_snapshots = get_unencrypted_volumes_and_snapshots()
 
-unattached_volumes_count = len(unattached_volumes)
-unencrypted_volumes_count = len(unencrypted_volumes)
-unencrypted_snapshots_count = len(unencrypted_snapshots)
+    unattached_volumes_count = len(unattached_volumes)
+    unencrypted_volumes_count = len(unencrypted_volumes)
+    unencrypted_snapshots_count = len(unencrypted_snapshots)
 
-unattached_volumes_size = sum([volume.size for volume in unattached_volumes])
-unencrypted_volumes_size = sum([volume.size for volume in unencrypted_volumes])
-unencrypted_snapshots_size = sum([snapshot.volume_size for snapshot in unencrypted_snapshots])
+    unattached_volumes_size = sum([volume.size for volume in unattached_volumes])
+    unencrypted_volumes_size = sum([volume.size for volume in unencrypted_volumes])
+    unencrypted_snapshots_size = sum([snapshot.volume_size for snapshot in unencrypted_snapshots])
 
-print(f"Number of unattached volumes: {unattached_volumes_count}")
-print(f"Total size of unattached volumes: {unattached_volumes_size} GB")
-print(f"Number of unencrypted volumes: {unencrypted_volumes_count}")
-print(f"Total size of unencrypted volumes: {unencrypted_volumes_size} GB")
-print(f"Number of unencrypted snapshots: {unencrypted_snapshots_count}")
-print(f"Total size of unencrypted snapshots: {unencrypted_snapshots_size} GB")
+    metrics = {
+        "unattached_volumes_count": unattached_volumes_count,
+        "unattached_volumes_size": unattached_volumes_size,
+        "unencrypted_volumes_count": unencrypted_volumes_count,
+        "unencrypted_volumes_size": unencrypted_volumes_size,
+        "unencrypted_snapshots_count": unencrypted_snapshots_count,
+        "unencrypted_snapshots_size": unencrypted_snapshots_size,
+    }
+
+    metrics_json = json.dumps(metrics)
+
+    # Write the metrics to a file
+    with open('metrics.json', 'w') as file:
+        file.write(metrics_json)
+
+    # Upload the file to an S3 bucket
+    bucket_name = 'your-bucket-name'  # Replace with your bucket name
+    s3_resource.Bucket(bucket_name).upload_file(Filename='metrics.json', Key='metrics.json')
+
+except Exception as e:
+    print(f"An error occurred: {e}")
